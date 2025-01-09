120c2d8680b8bba5f69bec25c5f4061e6660b7df
Tarkan Zarrouk <83801755+LarryTheFatCat@users.noreply.github.com>
2025-01-09 12:18:43 -0500

    Add Account class with constructor and toString method; update Main class for account management options

--- src/Account.java
+++ src/Account.java
@@ -11,6 +11,15 @@ import java.io.IOException;
  * Bank account allowing you to perform actions such as Creating an account, deleting an account, adding money... etc
  */
 public class Account {
+    private String accountNumber;
+    private String accountName;
+    private double balance;
+    // 
+    public Account(String accountName, String accountNumber) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = 0.00;
+    }
     // File creation object... (W3 Schools - https://www.w3schools.com/java/java_files_create.asp);
     public static String createAccount(String accountName) {
         try {
@@ -24,5 +33,10 @@ public class Account {
             System.out.println(e);
         }
         return "Successfully created " + accountName;
+    }
+    
+    @Override
+    public String toString() {
+        return "Account Number: " + accountNumber + "\n" + "Account Name: " + accountName + "\n" + "Balance: " + balance + "\n";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -14,37 +14,53 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int choice; // create initial choice value... not 0 or anything else
+        // initalize basic composition
+        Account bankAccount = new Account("Tarkan's Bank Account", "4506-1125-2455-9084");
         do {
             System.out.println("Hi, welcome to your personalized banking application");
-            System.out.println("Please Choose An Option:");
-            System.out.println("[0] - Exit");
-            System.out.println("[1] - Create an Account");
-            System.out.println("[2] - Deposit Money");
-            System.out.println("[3] - Withdraw Money");
-            System.out.println("[4] - Transfer Money");
-            System.out.println("[5] - View Transaction History");
-            choice = input.nextInt();
-            input.nextLine(); // flush
-
-            if(choice == 0) {
+            System.out.println("Please Choose an Option:");
+            System.out.println("[1] - Exit");
+            System.out.println("[2] - Account");
+            System.out.println("[3] Chequeing");
+            choice = Integer.parseInt(input.nextLine());
+            if(choice != 0) {
+                break;
+            }
+        } while(choice == 0);
+        do {
+            if(choice == 1) {
                 System.out.println("Successfully exited program");
                 System.exit(0); 
-            } else if(choice == 1) {
+            } else if(choice == 2) {
                 // Account class
-                System.out.println("Sure! We can create an account for you! 😊");
-                System.out.print("What should we call it: ");
-                String accountName = input.nextLine();
-                Account.createAccount(accountName);
-                System.out.println("Please Choose An Option:");
-                System.out.println("[0] - Exit");
-                System.out.println("[1] - Create an Account");
-                System.out.println("[2] - Deposit Money");
-                System.out.println("[3] - Withdraw Money");
-                System.out.println("[4] - Transfer Money");
-                System.out.println("[5] - View Transaction History");
-            }
-        } while (input.nextInt() != 0);
+                System.out.println("Welcome to Your Account Section!");
+                System.out.println("Please Choose an Option Below:");
+                System.out.println("[A] - Exit");
+                System.out.println("[B] - Add Account");
+                // @TODO: Implement Remove Account
+                System.out.println("[C] - Remove Account");
+                System.out.println("[B] - Account information");
+                // TODO: Implement Transaction history
+                System.out.println("[C] - Transaction History");
+                if(input.nextLine().equals("A")) {
+                    break;
+                }
+                if(input.nextLine().equals("B")) {
+                    System.out.println(bankAccount.toString());
+                    choice = 0;
+                }
 
+                if(choice == 0) {
+                    break;
+                }
+                // System.out.println("Sure! We can create an account for you! 😊");
+                // System.out.print("What    should we call it: ");
+                // String accountName = input.nextLine();
+                // Account.createAccount(accountName);
+                // System.out.println("Please Choose An Option:");
+                // System.out.println("[5] - View Transaction History");
+            }
+        } while (Integer.parseInt(input.nextLine()) != 0);
         // close scanner
         input.close();
     }
